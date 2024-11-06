7db065f9ba5aa23f490a3cf06ec5fd4a4f4a92f0
pQbas <percybrayan18@gmail.com>
2024-11-05 20:31:28 -0500

    resnet18 was added as default resnet model

--- resnet/model.py
+++ resnet/model.py
@@ -1,63 +1,107 @@
 import torch
 import torch.nn as nn
 
+
+class ResNetBlock(nn.Module):
+    def __init__(self, c_in, c_out, kernel_size, stride=1):
+        super(ResNetBlock, self).__init__()
+
+
+        self.w1 = nn.Sequential(
+            nn.Conv2d(c_in, c_out, kernel_size=kernel_size, stride=stride, padding=1, 
+                      bias=False),
+            nn.BatchNorm2d(c_out)
+        )
+
+        self.w2 = nn.Sequential(
+            nn.Conv2d(c_out, c_out, kernel_size=kernel_size, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(c_out)
+        )
+
+        self.use_identity = (c_in != c_out)
+        
+        if self.use_identity:
+            self.identity = nn.Conv2d(c_in, c_out, kernel_size=1, stride=stride, padding=0, 
+                                      bias=False)
+        else:
+            self.identity = nn.Identity()        
+
+    def forward(self, x):
+        
+        F = nn.Sequential(
+            self.w1,
+            nn.ReLU(),
+            self.w2)
+
+        I = self.identity
+        
+        return F(x) + I(x)
+
+
 class ResNet(nn.Module):
     def __init__(self, categories=10):
         super(ResNet, self).__init__()
 
-        self.stack1 = nn.Sequential(
-            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(96),
-            nn.MaxPool2d(kernel_size=3, stride=2)
-        )
-
-        self.stack2 = nn.Sequential(
-            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
-            nn.ReLU(),
-            nn.BatchNorm2d(256),
-            nn.MaxPool2d(kernel_size=3, stride=2)
-        )
-
-        self.stack3 = nn.Sequential(
-            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+        
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.conv2_1 = nn.Sequential(
+            ResNetBlock(64, 64, kernel_size=3),
             nn.ReLU()
         )
 
-        self.stack4 = nn.Sequential(
-            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+        self.conv2_2 = nn.Sequential(
+            ResNetBlock(64, 64, kernel_size=3),
             nn.ReLU()
         )
 
-        self.stack5 = nn.Sequential(
-            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(3,3)
-        )
-
-        self.stack6 = nn.Sequential(
-            nn.Linear(4096, 4096),
+        self.conv3_1 = nn.Sequential(
+            ResNetBlock(64, 128, kernel_size=3, stride=2),
             nn.ReLU()
         )
 
-        self.stack7 = nn.Sequential(
-            nn.Linear(4096, 4096),
+        self.conv3_2 = nn.Sequential(
+            ResNetBlock(128, 128, kernel_size=3),
             nn.ReLU()
         )
 
-        self.stack8 = nn.Sequential(
-            nn.Linear(4096, categories)
+        self.conv4_1 = nn.Sequential(
+            ResNetBlock(128, 256, kernel_size=3, stride=2),
+            nn.ReLU()
+        )
+
+        self.conv4_2 = nn.Sequential(
+            ResNetBlock(256, 256, kernel_size=3),
+            nn.ReLU()
+        )
+
+        self.conv5_1 = nn.Sequential(
+            ResNetBlock(256, 512, kernel_size=3, stride=2),
+            nn.ReLU()
+        )
+
+        self.conv5_2 = nn.Sequential(
+            ResNetBlock(512, 512, kernel_size=3),
+            nn.ReLU()
+        )
+
+        self.decode = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten(),
+            nn.Linear(512, categories)
         )
 
     def forward(self, x):
-        x = self.stack1(x)
-        x = self.stack2(x)
-        x = self.stack3(x)
-        x = self.stack4(x)
-        x = self.stack5(x)
-        x = torch.flatten(x, start_dim=1)
-        x = self.stack6(x)
-        x = self.stack7(x)
-        x = self.stack8(x)
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.conv2_1(x)
+        x = self.conv2_2(x)
+        x = self.conv3_1(x)
+        x = self.conv3_2(x)
+        x = self.conv4_1(x)
+        x = self.conv4_2(x)
+        x = self.conv5_1(x)
+        x = self.conv5_2(x)
+        x = self.decode(x)
         return x
-

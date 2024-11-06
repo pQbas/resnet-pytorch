84fb980b1168ce731e46cf42d16b2144f17f275c
pQbas <percybrayan18@gmail.com>
2024-11-05 20:35:21 -0500

    test for resnetblock added

--- tests/test_resnetblock.py
+++ tests/test_resnetblock.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 from resnet.model import ResNetBlock
 
-def test_model():
+def test_resnetblock():
    
-   model = ResNetBlock(c_in=10, c_out=10, k_size=3)
+   model = ResNetBlock(c_in=10, c_out=10, kernel_size=3)
    input  = torch.rand([4, 10, 224, 224])
    output = model(input)
 
